d73462da5e2b7f22b0653016248ab6fe2137b569
Nihal Deb <nihaldeb2006@gmail.com>
2025-05-12 23:23:06 +0800

    add relative filepath

--- etl/load/loader.py
+++ etl/load/loader.py
@@ -4,7 +4,7 @@ from load.row_loader import load_report_batch_row
 
 def load(report_batch_dataframe : pd.DataFrame) -> None:
     
-    conn = sqlite3.connect(r'C:\Projects\DB_report_test2\test.db')
+    conn = sqlite3.connect(r'.\etl\test.db')
     cursor = conn.cursor()
     
     report_batch_dataframe = report_batch_dataframe.apply(lambda row: load_report_batch_row(row, cursor=cursor, conn=conn), axis=1)

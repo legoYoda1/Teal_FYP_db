ecb540581e31decc37c858d4a67027d14edcc374
legoYoda1 <XU01.23@ichat.sp.edu.sg>
2025-05-19 13:48:00 +0800

    update loading of data for each row from batch, foreign keys are loaded into fact table

--- etl/load/row_loader.py
+++ etl/load/row_loader.py
@@ -5,6 +5,25 @@ foreign_key_id = {
     # 'supervisor_dim' : None
 }
 
+def load_dim(dim : str, fields : tuple, values: tuple, foreign_key_id : dict, cursor : sqlite3.Cursor, conn : None) -> None:
+    try:
+        fields_string = ', '.join(fields)
+        values_placeholder_string = ', '.join(['?'] * len(fields))
+        
+        cursor.execute(f'''
+            INSERT INTO {dim} ({fields_string})
+            VALUES ({values_placeholder_string})
+        ''', values)
+
+        row_id = cursor.lastrowid
+        foreign_key_id[dim] = row_id
+                    
+        conn.commit()
+    except KeyError as e:
+        print("DB error: ", e)
+        raise
+            
+
 def load_dim_non_repeatable_field(value : any, dim : str, field : str, foreign_key_id : dict, cursor : sqlite3.Cursor, conn : None) -> None:
     try:
             cursor.execute(f'''
@@ -23,7 +42,7 @@ def load_dim_non_repeatable_field(value : any, dim : str, field : str, foreign_k
             ''', (value,))
 
             row_id = (cursor.fetchone())[0]
-            foreign_key_id['supervisor_dim'] = row_id
+            foreign_key_id[dim] = row_id
             # print(row_id)
             
             conn.commit()
@@ -35,50 +54,33 @@ def load_report_batch_row(report_batch_row : pd.Series, cursor : sqlite3.Cursor,
 
     print("bruh")
     try:
-        # Inserting data into supervisor_dim table
-        # cursor.execute('''
-        #     INSERT INTO supervisor_dim (name)
-        #     VALUES (?)
-        # ''', (report_batch_row['Supervised_by'],))  
-        # conn.commit()
-        # Perform the operation
         load_dim_non_repeatable_field(report_batch_row['Supervised_by'], 'supervisor_dim', 'name', foreign_key_id, cursor, conn)
         load_dim_non_repeatable_field(report_batch_row['Inspected_by'], 'inspector_dim', 'name', foreign_key_id, cursor, conn)
 
-        # # Inserting data into inspector_dim table
-        # cursor.execute('''
-        #     INSERT INTO inspector_dim (name) 
-        #     VALUES (?)
-        # ''', (report_batch_row['Inspected_by'],))  # Replace with actual name
-        # conn.commit()
-
-        # # Inserting data into reported_via_dim table
-        # cursor.execute('''
-        #     INSERT INTO reported_via_dim (method, agency, date_id, time_id) 
-        #     VALUES (?, ?, ?, ?)
-        # ''', (report_batch_row['Reported_via__method'], report_batch_row['Reported_via__agency'], report_batch_row['Reported_via__date'], report_batch_row['Reported_via__time']))  # Replace with actual date_id and time_id
-        # conn.commit()
-
-        # # Inserting data into acknowledgement_dim table
-        # cursor.execute('''
-        #     INSERT INTO acknowledgement_dim (method, date_id, time_id) 
-        #     VALUES (?, ?, ?)
-        # ''', (report_batch_row['Reported_via__method'], report_batch_row['Reported_via__date'], report_batch_row['Reported_via__time']))
-        # conn.commit()
-
-        # # Inserting data into lta_verified_by_dim table
-        # cursor.execute('''
-        #     INSERT INTO lta_verified_by_dim (name, date_id, instructions, wso_no) 
-        #     VALUES (?, ?, ?, ?)
-        # ''', (report_batch_row['Defects_verified_by__name'], report_batch_row['Defects_verified_by__date'], report_batch_row['Instructions'], report_batch_row['WSO_no']))  # Replace with actual values
-        # conn.commit()
-
-        # # Inserting data into contractor_acknowledged_received_by_dim table
-        # cursor.execute('''
-        #     INSERT INTO contractor_acknowledged_received_by_dim (name, date_id) 
-        #     VALUES (?, ?)
-        # ''', (report_batch_row['Acknowledged_and_received_by__name'], report_batch_row['Acknowledged_and_received_by__date']))  # Replace with actual date_id
-        # conn.commit()
+        load_dim(dim='reported_via_dim', fields=('method', 'agency', 'date_id', 'time_id'), 
+                 values=(report_batch_row['Reported_via__method'], report_batch_row['Reported_via__agency'], 
+                         report_batch_row['Reported_via__date'], report_batch_row['Reported_via__time']),
+                 foreign_key_id=foreign_key_id, 
+                 cursor=cursor, conn=conn
+                 )
+        load_dim(dim='acknowledgement_dim', fields=('method', 'date_id', 'time_id'), 
+                 values=(report_batch_row['Reported_via__method'], report_batch_row['Reported_via__date'], 
+                         report_batch_row['Reported_via__time']),
+                 foreign_key_id=foreign_key_id, 
+                 cursor=cursor, conn=conn
+                 )
+        load_dim(dim='lta_verified_by_dim', fields=('name', 'date_id', 'instructions', 'wso_no'), 
+                 values=(report_batch_row['Defects_verified_by__name'], report_batch_row['Defects_verified_by__date'], 
+                         report_batch_row['Instructions'], report_batch_row['WSO_no']),
+                 foreign_key_id=foreign_key_id, 
+                 cursor=cursor, conn=conn
+                 )
+        load_dim(dim='contractor_acknowledged_received_by_dim', fields=('name', 'date_id'), 
+                 values=(report_batch_row['Acknowledged_and_received_by__name'], 
+                         report_batch_row['Acknowledged_and_received_by__date']),
+                 foreign_key_id=foreign_key_id, 
+                 cursor=cursor, conn=conn
+                 )
 
         # Inserting data into report_fact table
         cursor.execute('''
@@ -87,13 +89,23 @@ def load_report_batch_row(report_batch_row : pd.Series, cursor : sqlite3.Cursor,
             repeated_defect, description, quantity, measurement, cause_of_defect, reccomendation) 
             VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
         ''', (
-            report_batch_row['Defect_ref_no'], report_batch_row['Date'], '1', '1', 
-            foreign_key_id['supervisor_dim'], foreign_key_id['supervisor_dim'], 1, 1, 
-            1, 1, report_batch_row['Repeated_defect'], 
-            report_batch_row['Description'], report_batch_row['Quantity'], report_batch_row['Measurement'], 
-            report_batch_row['Cause_of_defect'], report_batch_row['Recommendation']
+            report_batch_row['Defect_ref_no'], 
+            report_batch_row['Date'], 
+            '1', 
+            '1', 
+            foreign_key_id['supervisor_dim'], 
+            foreign_key_id['inspector_dim'], 
+            foreign_key_id['reported_via_dim'], 
+            foreign_key_id['acknowledgement_dim'], 
+            foreign_key_id['lta_verified_by_dim'], 
+            foreign_key_id['contractor_acknowledged_received_by_dim'], 
+            report_batch_row['Repeated_defect'], 
+            report_batch_row['Description'], 
+            report_batch_row['Quantity'], 
+            report_batch_row['Measurement'], 
+            report_batch_row['Cause_of_defect'], 
+            report_batch_row['Recommendation']
         ))  # Replace with actual values
-        
         conn.commit()
 
         # cursor.execute('''
@@ -103,3 +115,4 @@ def load_report_batch_row(report_batch_row : pd.Series, cursor : sqlite3.Cursor,
     # pass
     except KeyError as e:
         print('Db error: ', e)
+        raise
